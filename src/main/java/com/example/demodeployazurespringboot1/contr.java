bd920bed826b951a9338e2340be56a514b695184
Vladislavskd13sdf <koshulkov@gmail.com>
2022-07-21 13:33:31 +0300

    update

--- src/main/java/com/example/demodeployazurespringboot1/contr.java
+++ src/main/java/com/example/demodeployazurespringboot1/contr.java
@@ -1,2 +1,18 @@
-package com.example.demodeployazurespringboot1;public class contr {
+package com.example.demodeployazurespringboot1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class contr
+{
+
+    @GetMapping("/")
+    public String gg()
+    {
+        return "Good)";
+    }
 }
